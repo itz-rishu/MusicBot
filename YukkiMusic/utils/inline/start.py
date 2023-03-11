264abc9d6ff65ebdee9268008689f4284d04a564
Rishu <67968330+itz-rishu@users.noreply.github.com>
2023-03-11 20:24:18 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -59,14 +59,15 @@ def start_pannel(_):
 
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-
-buttons = [
+buttons.append(       
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
+)
+buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_8"], callback_data="settings_back_helper"
